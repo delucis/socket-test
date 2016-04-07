0a1e418dad40dd832407a1e9745881ae5d9fec14
delucis <swithinbank@gmail.com>
2016-04-06 20:53:11 -0400

    Fix sendfile method.

--- index.js
+++ index.js
@@ -3,11 +3,14 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 app.get('/', function(req, res){
-  res.sendFile('index.html');
+  res.sendfile('index.html');
 });
 
 io.on('connection', function(socket){
   console.log('A user connected.');
+  socket.on('disconnect', function(){
+    console.log('A user disconnected.');
+  });
 });
 
 http.listen(3000, function(){
