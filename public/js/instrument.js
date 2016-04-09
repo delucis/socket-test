f56143bf4aaa6be8b59598d162241ef424f6f433
delucis <swithinbank@gmail.com>
2016-04-09 16:10:27 -0400

    Display message from performer in instrument page.

--- public/js/instrument.js
+++ public/js/instrument.js
@@ -9,6 +9,6 @@ $(document).ready(function() {
     $('#messages').append($('<li>').text(msg));
   });
   socket.on('menu select', function(sel){
-    console.log("selected item: " + sel);
+    $('#num').text(sel);
   });
 });
